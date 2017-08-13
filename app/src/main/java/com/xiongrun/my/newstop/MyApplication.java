2fdb0fa62e7c16e69d24290743bf3297b75f05c5
XiongRun <755949102@qq.com>
2017-08-13 22:33:19 +0800

    初始环境搭建，准备使用RxJava+MVP+Dagger2+Retrofit在原来的项目进行改进

---
+++ app/src/main/java/com/xiongrun/my/newstop/MyApplication.java
@@ -0,0 +1,11 @@
+package com.xiongrun.my.newstop;
+
+import android.app.Application;
+
+/**
+ * @author XiongRun
+ * @time 2017/8/13 21:51
+ */
+
+public class MyApplication extends Application {
+}
